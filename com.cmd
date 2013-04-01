b2674f710a45fd86075ae2ea303e1b84aca716b7
seagg <proudsea@yeah.net>
2013-04-01 19:58:12 +0800

    helo

--- com.cmd
+++
@@ -1,4 +0,0 @@
-java -jar compiler.jar js\combined\alloyimage.js --js_output_file=js\combined\temp.js
-del js\combined\alloyimage.js
-rename js\combined\temp.js alloyimage.js
-del js\combined\temp.js
